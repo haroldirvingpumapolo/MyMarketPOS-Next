feb5e7237e82c3c96472d87d73e135ffca3d8576
HaroldIrvingPuma <pumapoloharold123@gmail.com>
2025-10-05 20:02:01 -0500

    feat: add shadcn UI, lucide icons, and sidebar component

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,73 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+  {
+    title: "Counter ",
+    url: "counter",
+    icon: Home,
+  },
+  {
+    title: "Products",
+    url: "products",
+    icon: Inbox,
+  },
+  {
+    title: "Receipts",
+    url: "receipts",
+    icon: Calendar,
+  },
+  {
+    title: "Analytics",
+    url: "analytics",
+    icon: Search,
+  },
+  {
+    title: "Reports",
+    url: "reports",
+    icon: Settings,
+  },
+  {
+    title: "Settings",
+    url: "settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
